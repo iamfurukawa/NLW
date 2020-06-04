07d7f1fb3f3f01cc4086ebe9c3c9e9562b32d2c2
Vinícius Furukawa Carvalho <vinicius_furuka@hotmail.com.br>
2020-06-04 19:03:09 -0300

    Fix combobox

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Feather as Icon } from '@expo/vector-icons';
-import { View, Image, StyleSheet, Text, ImageBackground, KeyboardAvoidingView, Platform } from 'react-native';
+import { View, Image, StyleSheet, Text, ImageBackground, KeyboardAvoidingView, Platform, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton, TextInput } from 'react-native-gesture-handler';
 import RNPickerSelect from 'react-native-picker-select';
@@ -16,11 +16,11 @@ interface IBGECityResponse {
 
 const Home = () => {
     const navigation = useNavigation();
-    const [uf, setUf] = useState('');
-    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('0');
+    const [city, setCity] = useState('0');
 
-    const [ufs, setUfs] = useState<string[]>(['0']);
-    const [cities, setCities] = useState<string[]>(['0']);
+    const [ufs, setUfs] = useState<string[]>([]);
+    const [cities, setCities] = useState<string[]>([]);
 
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
 
@@ -44,7 +44,17 @@ const Home = () => {
     }, [uf]);
 
     function handleNavigateToPoints(uf: string, city: string) {
-        navigation.navigate('Points', {uf, city});
+      if(city === '0' || uf === '0') {
+        Alert.alert('Espere!', 'Selecione uma localidade para continuar.')
+        return;
+      }
+      navigation.navigate('Points', {uf, city});
+    }
+
+    function handleChangeUf(uf: string) {
+      setUf(uf);
+      setCity('0');
+      setCities([]);
     }
 
     return (
@@ -60,11 +70,15 @@ const Home = () => {
 
           <View style={styles.footer}>
               <RNPickerSelect
-                onValueChange={(uf) => setUf(uf)}
+                onValueChange={(uf) => handleChangeUf(uf)}
+                style={select}
+                Icon={() => <Icon name='chevron-down' size={30} color='#777'/>}
                 placeholder={{
                   label: "Escolha um estado",
                   value: '0',
+                  key: '0'
                 }}
+                value={uf}
                 items={
                   ufs.map(uf => ({value: uf, label: uf, key: uf}))
                 }
@@ -72,10 +86,14 @@ const Home = () => {
 
               <RNPickerSelect
                 onValueChange={(city) => setCity(city)}
+                style={select}
+                Icon={() => <Icon name='chevron-down' size={30} color='#777'/>}
                 placeholder={{
                   label: "Escolha uma cidade",
                   value: '0',
+                  key: '0'
                 }}
+                value={city}
                 items={
                   cities.map(city => ({value: city, label: city, key: city}))
                 }
@@ -93,81 +111,86 @@ const Home = () => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 32
-    },
-  
-    main: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-  
-    title: {
-      color: '#322153',
-      fontSize: 32,
-      fontFamily: 'Ubuntu_700Bold',
-      maxWidth: 260,
-      marginTop: 64,
-    },
-  
-    description: {
-      color: '#6C6C80',
-      fontSize: 16,
-      marginTop: 16,
-      fontFamily: 'Roboto_400Regular',
-      maxWidth: 260,
-      lineHeight: 24,
-    },
-  
-    footer: {},
-  
-    select: {
-      fontSize: 16,
-      paddingVertical: 12,
-      paddingHorizontal: 10,
-      borderWidth: 1,
-      borderColor: 'gray',
-      borderRadius: 4,
-      color: 'black',
-      paddingRight: 30,
-    },
-  
-    input: {
+  container: {
+    flex: 1,
+    padding: 32,
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#322153',
+    fontSize: 32,
+    fontFamily: 'Ubuntu_700Bold',
+    maxWidth: 260,
+    marginTop: 64,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 16,
+    fontFamily: 'Roboto_400Regular',
+    maxWidth: 260,
+    lineHeight: 24,
+  },
+
+  footer: {},
+
+  select: {},
+
+  button: {
+    backgroundColor: '#34CB79',
+    height: 60,
+    flexDirection: 'row',
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+
+  buttonIcon: {
+    height: 60,
+    width: 60,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#FFF',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  },
+});
+
+  const select = StyleSheet.create({
+    inputIOS: {
       height: 60,
       backgroundColor: '#FFF',
-      borderRadius: 10,
       marginBottom: 8,
-      paddingHorizontal: 24,
-      fontSize: 16,
-    },
-  
-    button: {
-      backgroundColor: '#34CB79',
-      height: 60,
-      flexDirection: 'row',
+      color: '#777',
+      paddingLeft: 10,
       borderRadius: 10,
-      overflow: 'hidden',
-      alignItems: 'center',
-      marginTop: 8,
+      overflow: 'hidden'
     },
-  
-    buttonIcon: {
+    inputAndroid: {
       height: 60,
-      width: 60,
-      backgroundColor: 'rgba(0, 0, 0, 0.1)',
-      justifyContent: 'center',
-      alignItems: 'center'
+      backgroundColor: '#FFF',
+      marginBottom: 8,
+      color: '#777',
+      paddingLeft: 10
     },
-  
-    buttonText: {
-      flex: 1,
-      justifyContent: 'center',
-      textAlign: 'center',
-      color: '#FFF',
-      fontFamily: 'Roboto_500Medium',
-      fontSize: 16,
-    }
+    iconContainer: {
+      top: 15,
+      right: 10
+    },
   });
 
 export default Home;
